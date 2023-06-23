bd38727efa99befd39fea27b7c4293a24c19fa8d
arunci <96084440+arunci@users.noreply.github.com>
2023-06-24 01:17:41 +0200

    Start again

--- src/AddThoughtForm.js
+++ src/AddThoughtForm.js
@@ -1,37 +1,15 @@
-import React, { useState } from "react";
-import { generateId, getNewExpirationTime } from "./utilities";
+import React from 'react';
+import { generateId, getNewExpirationTime } from './utilities';
 
 export function AddThoughtForm(props) {
-  const [text, setText] = useState("");
-
-  const handleTextChange = ({ target }) => {
-    const { value } = target;
-    setText(value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const thought = {
-      id: generateId(),
-      text: text,
-      expiresAt: getNewExpirationTime(),
-    };
-    if (text.length > 0) {
-      props.addThought(thought);
-    }
-    setText("");
-  };
-
   return (
-    <form onSubmit={handleSubmit} className="AddThoughtForm">
-      <input
-        value={text}
-        onChange={handleTextChange}
-        type="text"
-        aria-label="What's on your mind?"
-        placeholder="What's on your mind?"
-      />
-      <input type="submit" value="Add" />
-    </form>
+      <form className="AddThoughtForm">
+        <input
+            type="text"
+            aria-label="What's on your mind?"
+            placeholder="What's on your mind?"
+        />
+        <input type="submit" value="Add" />
+      </form>
   );
 }
--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
-import { AddThoughtForm } from "./AddThoughtForm";
-import { Thought } from "./Thought";
-import { generateId, getNewExpirationTime } from "./utilities";
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import { AddThoughtForm } from './AddThoughtForm';
+import { Thought } from './Thought';
+import { generateId, getNewExpirationTime } from './utilities';
 
 export default function App() {
   const [thoughts, setThoughts] = useState([
     {
       id: generateId(),
-      text: "This is a place for your passing thoughts.",
+      text: 'This is a place for your passing thoughts.',
       expiresAt: getNewExpirationTime(),
     },
     {
@@ -18,32 +18,19 @@ export default function App() {
     },
   ]);
 
-  const addThought = (thought) => {
-    setThoughts((thoughts) => [thought, ...thoughts]);
-  };
-
-  const removeThought = (thoughtIdToRemove) => {
-    setThoughts((thoughts) =>
-      thoughts.filter((thought) => thought.id !== thoughtIdToRemove)
-    );
-  };
   return (
-    <div className="App">
-      <header>
-        <h1>Passing Thoughts</h1>
-      </header>
-      <main>
-        <AddThoughtForm addThought={addThought} />
-        <ul className="thoughts">
-          {thoughts.map((thought) => (
-            <Thought
-              key={thought.id}
-              thought={thought}
-              removeThought={removeThought}
-            />
-          ))}
-        </ul>
-      </main>
-    </div>
+      <div className="App">
+        <header>
+          <h1>Passing Thoughts</h1>
+        </header>
+        <main>
+          <AddThoughtForm />
+          <ul className="thoughts">
+            {thoughts.map((thought) => (
+                <Thought key={thought.id} thought={thought} />
+            ))}
+          </ul>
+        </main>
+      </div>
   );
 }
